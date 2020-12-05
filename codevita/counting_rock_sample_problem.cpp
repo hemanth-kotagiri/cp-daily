dc131a3b51ca5d7c33ce7cac6dc47244355e4908
hemanth@wsl2 <hemanth.kotagiri43@gmail.com>
2020-12-05 23:58:14 +0530

    Add codevita counting_rock.cpp

---
+++ codevita/counting_rock_sample_problem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    long R, S, i;
+    cin >> S >> R;
+    vector<int> sizes;
+    for(i = 0; i < S; i++) {
+        long curr;
+        cin >> curr;
+        sizes.push_back(curr);
+    }
+}
